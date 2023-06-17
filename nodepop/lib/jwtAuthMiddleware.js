47ec472f99ec3642ac44dbd85fdbbdeae8aaea80
Antonio Luis Martinez <antonillo_1995@hotmail.com>
2023-06-17 13:54:04 +0200

    Modified token time expire to 1 minute to prove token expired error

--- nodepop/lib/jwtAuthMiddleware.js
+++ nodepop/lib/jwtAuthMiddleware.js
@@ -4,23 +4,29 @@ const createError = require('http-errors');
 // Módulo que exporta un middleware
 module.exports = async (req, res, next) => {
   try {
+    // Recoger el jwtToken de la cabecera, o del body o de la query string
+    const jwtToken = req.get('Authorization') || req.body.jwt || req.query.jwt;
 
-  // Recoger el jwtToken de la cabecera, o del body o de la query string
-  const jwtToken = req.get('Authorization') || req.body.jwt || req.query.jwt;
+    // Comprobar que me han mandado un jwtToken
+    if (!jwtToken) {
+      const error = createError(401, 'No token provided')
+      next(error);
+      return;
+    }
 
-  // Comprobar que me han mandado un jwtToken
-  if (!jwtToken) {
-    const error = createError(401, 'No token provided')
-    next(error);
-    return;
-  }
-
-  // Comprobar que el token es válido
-  const payload = jwt.verify(jwtToken, process.env.JWT_SECRET)
-
-  req.logedApiUser = payload._id;
-
-  next();
+    // Comprobar que el token es válido
+    try {
+      const payload = jwt.verify(jwtToken, process.env.JWT_SECRET)
+      req.logedApiUser = payload._id;
+      next();
+    } catch (err) {
+      if (err.message === 'jwt expired') {
+        const error = createError(401, 'Token expired');
+        next(error);
+        return;
+      }
+      throw err;
+    }
 
   } catch(err) {
     if (err.message === 'invalid signature') {
